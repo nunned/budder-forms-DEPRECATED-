a2ee40698ca6715c8ab12da21df0707f95dd4557
nunned <chanonac@outlook.com>
2023-09-28 00:42:02 -0500

    fixed todo list for next session

--- README.md
+++ README.md
@@ -3,11 +3,14 @@
 # TODO list
 
 - Fix styling so placeholder text matches
-- Replace suggestions in input with autocomplete
+- Replace suggestions in input with autocomplete [almost done]
+- fix plant group names and plant batch names
 
 # Abbreviations
 
 - ChangePBStrains = Change Plant Batches Strains
     - PB = Plant Batches
 - CreateIPP = Create Immature Plant Packages
-    - IPP = Immature Plant Package
+    - IPP = Immature Plant Packages
+- ChangeIPGP = Change Immature Plant Growth Phase
+    - IPGP = Immature Plant Growth Phase
---
+++ src/components/forms/immature/ChangeIPGP.jsx
@@ -0,0 +1,111 @@
+import "../template_form.css";
+import Form_header from "../folder-comp/form_header";
+import CustomDropdown from "../folder-comp/CustomDropdown";
+import { useState } from "react";
+import DatePicker from "../folder-comp/datepicker";
+import AutoComplete from "../../AutoComplete";
+
+function ChangeIPGP() {
+  const [formData, setFormData] = useState({
+    groupName: "",
+    dropdownValue: "",
+    plantCount: "",
+    strain: "",
+    dateValue: "",
+    location: "",
+  });
+
+  //These need to be made dynamic
+  const strains = ["Apple Fritter", "RAW", "Forum", "Runtz"]; // Add more strains as necessary
+
+  const locations = ["BREEDING", "CLONE", "DRYING", "MOTHER", "VEGETATIVE"];
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: Push formData to your backend
+    console.log(formData);
+
+    const newWindow = window.open("", "_blank");
+    newWindow.document.write(`<pre>${JSON.stringify(formData, null, 2)}</pre>`);
+  };
+
+  return (
+    <div className="form-wrap">
+      <div className="form-container">
+        <Form_header text="Create Plantings" />
+        <div className="form-content">
+          <form onSubmit={handleSubmit}>
+            <div className="itm-list">
+              <div className="itm-container">
+                <p>Group Name</p>
+                <input
+                  type="text"
+                  name="groupName"
+                  placeholder="ex. B. Kush 5-30"
+                  value={formData.groupName}
+                  onChange={handleChange}
+                />
+              </div>
+              <>
+                <CustomDropdown
+                  text="Plants Type"
+                  options={["Clone", "Seed"]}
+                  name="dropdownValue"
+                  onChange={handleChange}
+                />
+              </>
+              <div className="itm-container">
+                <p>Plants Count</p>
+                <input
+                  type="text"
+                  name="plantCount"
+                  placeholder="ex. 100"
+                  value={formData.plantCount}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="itm-container">
+                <p>Strain</p>
+                <AutoComplete
+                  options={strains}
+                  onChange={(selectedValue) => {
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      strain: selectedValue,
+                    }));
+                  }}
+                />
+              </div>
+              <DatePicker onChange={handleChange} />
+              <div className="itm-container">
+                <p>Location</p>
+                <AutoComplete
+                  options={locations}
+                  onChange={(selectedValue) => {
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      location: selectedValue,
+                    }));
+                  }}
+                />
+              </div>
+            </div>
+            <button type="submit" className="submit-button">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChangeIPGP;
